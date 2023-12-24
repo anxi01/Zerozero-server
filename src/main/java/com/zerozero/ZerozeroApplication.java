5ed4421840fa3eee75c57e6602ab51dd93a2c83d
anxi01 <tjdals2893@naver.com>
2023-12-25 02:11:24 +0900

    init : 스프링 프로젝트 생성

---
+++ src/main/java/com/zerozero/ZerozeroApplication.java
@@ -0,0 +1,13 @@
+package com.zerozero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerozeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerozeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerozero/ZerozeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerozero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerozeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
