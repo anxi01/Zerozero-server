dc1cd58f893a5528f43b188880e0b2130fcc42d5
anxi01 <tjdals2893@naver.com>
2024-01-05 01:58:33 +0900

    [chore] : jasypt encryptkey 값 설정

--- src/main/java/com/zerozero/config/jasypt/JasyptConfig.java
+++ src/main/java/com/zerozero/config/jasypt/JasyptConfig.java
@@ -4,6 +4,7 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,12 +12,15 @@ import org.springframework.context.annotation.Configuration;
 @EnableEncryptableProperties
 public class JasyptConfig {
 
+  @Value("${jasypt.encryptor.password}")
+  private String encryptKey;
+
   @Bean("jasyptStringEncryptor")
   public StringEncryptor stringEncryptor() {
     PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
     SimpleStringPBEConfig config = new SimpleStringPBEConfig();
 
-    config.setPassword(System.getProperty("jasypt.encryptor.password"));
+    config.setPassword(encryptKey);
     config.setAlgorithm("PBEWithMD5AndDES");
     config.setPoolSize("1");
     encryptor.setConfig(config);
