79e5f48d177520d2eca254d25c3fe82e195554d1
anxi01 <tjdals2893@naver.com>
2024-01-05 21:18:21 +0900

    fix : ec2 배포시 테스트 오류 수정

--- src/test/java/com/zerozero/ZerozeroApplicationTests.java
+++ src/test/java/com/zerozero/ZerozeroApplicationTests.java
@@ -1,13 +1,8 @@
 package com.zerozero;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ZerozeroApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
