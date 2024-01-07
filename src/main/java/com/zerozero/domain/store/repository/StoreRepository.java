86a12db536ad4e8c3e4d3ae38e194ebf60277a75
anxi01 <tjdals2893@naver.com>
2024-01-08 02:49:18 +0900

    feat : 판매점 수를 조회하는 쿼리 생성

--- src/main/java/com/zerozero/domain/store/repository/StoreRepository.java
+++ src/main/java/com/zerozero/domain/store/repository/StoreRepository.java
@@ -4,8 +4,12 @@ import com.zerozero.domain.store.domain.Store;
 import com.zerozero.domain.user.domain.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
   List<Store> findStoresByUser(User user);
+
+  @Query("SELECT s.user.id, COUNT(s) as storeCount FROM Store s GROUP BY s.user.id ORDER BY storeCount DESC")
+  List<Object[]> countStoresByUserId();
 }
