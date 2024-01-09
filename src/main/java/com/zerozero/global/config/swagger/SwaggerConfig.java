5b2fbeffa0f1b95867cfdac9da14c61c974d3635
anxi01 <tjdals2893@naver.com>
2024-01-09 20:27:08 +0900

    feat : swagger config 생성

---
+++ src/main/java/com/zerozero/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.zerozero.global.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .components(new Components())
+        .info(apiInfo());
+  }
+
+  private Info apiInfo() {
+    return new Info()
+        .title("Zerozero API")
+        .description("Zerozero : API 명세서")
+        .version("v1.0.0");
+  }
+}
