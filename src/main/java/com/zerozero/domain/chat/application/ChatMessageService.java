ffe18fac3affc1b608673203433c2faaeef66a9b
anxi01 <tjdals2893@naver.com>
2024-02-07 20:42:05 +0900

    refactor : 메시지 저장시 nickname을 통해 조회

--- src/main/java/com/zerozero/domain/chat/application/ChatMessageService.java
+++ src/main/java/com/zerozero/domain/chat/application/ChatMessageService.java
@@ -2,6 +2,7 @@ package com.zerozero.domain.chat.application;
 
 import com.zerozero.domain.chat.domain.ChatMessage;
 import com.zerozero.domain.chat.domain.ChatRoom;
+import com.zerozero.domain.chat.dto.request.ChatMessageRequest;
 import com.zerozero.domain.chat.dto.response.ChatMessageResponse;
 import com.zerozero.domain.chat.repository.ChatMessageRepository;
 import com.zerozero.domain.chat.repository.ChatRoomRepository;
@@ -19,17 +20,22 @@ public class ChatMessageService {
   private final ChatMessageRepository chatMessageRepository;
   private final UserRepository userRepository;
 
-  public void saveMessage(Long chatRoomId, String message, Long userId) {
+  public ChatMessageResponse saveMessage(Long chatRoomId, ChatMessageRequest request) {
     ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(
         () -> new IllegalArgumentException("해당 ChatRoom이 존재하지 않습니다. chatRoomId = " + chatRoomId));
-    User user = userRepository.findById(userId)
+
+    User user = userRepository.findByNickname(request.getSender())
         .orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+
     ChatMessage chatMessage = ChatMessage.builder()
         .chatRoom(chatRoom)
-        .message(message)
+        .message(request.getContent())
         .user(user)
         .build();
+
     chatMessageRepository.save(chatMessage);
+
+    return new ChatMessageResponse(chatMessage);
   }
 
   public List<ChatMessageResponse> getMessage(Long roomId) {
