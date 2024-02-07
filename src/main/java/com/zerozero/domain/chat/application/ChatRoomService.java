ce69e6527641df25a3b42545438df0f084a5a639
anxi01 <tjdals2893@naver.com>
2024-02-07 20:40:59 +0900

    refactor : userId 대신 username을 이용해 조회

--- src/main/java/com/zerozero/domain/chat/application/ChatRoomService.java
+++ src/main/java/com/zerozero/domain/chat/application/ChatRoomService.java
@@ -1,15 +1,12 @@
 package com.zerozero.domain.chat.application;
 
 import com.zerozero.domain.chat.domain.ChatRoom;
-import com.zerozero.domain.chat.dto.request.ChatRoomRequest;
 import com.zerozero.domain.chat.dto.response.ChatRoomResponse;
 import com.zerozero.domain.chat.repository.ChatRoomRepository;
 import com.zerozero.domain.user.domain.User;
 import com.zerozero.domain.user.repository.UserRepository;
-import java.security.Principal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,28 +18,14 @@ public class ChatRoomService {
   private final UserRepository userRepository;
 
   @Transactional
-  public void enterChatRoom(Long roomId, Long userId) {
-    User user = userRepository.findById(userId)
+  public void enterChatRoom(Long roomId, String username) {
+    User user = userRepository.findByNickname(username)
         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
 
     ChatRoom chatRoom = chatRoomRepository.findById(roomId)
         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 채팅방입니다."));
 
     user.enterChatRoom(chatRoom);
-  }
-
-  @Transactional
-  public void exitChatRoom(Principal connectedUser) {
-
-    User user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
-    user.exitChatRoom();
-    userRepository.save(user);
-  }
-
-  /** ChatRoom 생성 */
-  public ChatRoomResponse createChatRoom(ChatRoomRequest requestDto) {
-    ChatRoom chatRoom = chatRoomRepository.save(requestDto.toEntity());
-    return new ChatRoomResponse(chatRoom);
   }
 
   public List<ChatRoomResponse> getAllChatRoom() {
