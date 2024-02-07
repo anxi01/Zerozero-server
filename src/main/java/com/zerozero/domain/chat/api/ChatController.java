1096ff902817a3b84d7358911a213b628c781b7f
anxi01 <tjdals2893@naver.com>
2024-02-07 20:39:56 +0900

    feat : 채팅 controller 채팅방 입장 및 메시지 전송 api 구현

--- src/main/java/com/zerozero/domain/chat/api/ChatController.java
+++ src/main/java/com/zerozero/domain/chat/api/ChatController.java
@@ -3,75 +3,46 @@ package com.zerozero.domain.chat.api;
 import com.zerozero.domain.chat.application.ChatMessageService;
 import com.zerozero.domain.chat.application.ChatRoomService;
 import com.zerozero.domain.chat.dto.request.ChatMessageRequest;
-import com.zerozero.domain.chat.dto.request.ChatRoomRequest;
 import com.zerozero.domain.chat.dto.response.ChatMessageResponse;
 import com.zerozero.domain.chat.dto.response.ChatRoomResponse;
-import com.zerozero.domain.user.domain.User;
-import java.security.Principal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/chat")
 public class ChatController {
 
   private final ChatRoomService chatRoomService;
   private final ChatMessageService chatMessageService;
 
-  /* 채팅방 생성 */
-  @PostMapping
-  public ResponseEntity<ChatRoomResponse> createChatRoom(@RequestBody ChatRoomRequest request) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(chatRoomService.createChatRoom(request));
-  }
-
   /* 채팅방 전체 조회 */
-  @GetMapping("/list")
+  @GetMapping("/chat/list")
   public ResponseEntity<List<ChatRoomResponse>> getAllChatRoom() {
     return ResponseEntity.ok(chatRoomService.getAllChatRoom());
   }
 
-  @MessageMapping("/send-message")
-  @SendTo("/topic/chat")
-  public void sendMessage(ChatMessageRequest request) {
-    Long userId = request.getUserId();
-    Long roomId = request.getRoomId();
-    String message = request.getMessage();
-
-    chatRoomService.enterChatRoom(roomId, userId);
-    chatMessageService.saveMessage(roomId, message, userId);
+  /* 채팅방 입장 */
+  @MessageMapping("/chat.addUser")
+  @SendTo("/topic/{roomId}")
+  public void addUser(@DestinationVariable Long roomId, @Payload ChatMessageRequest request,
+      SimpMessageHeaderAccessor headerAccessor) {
+    chatRoomService.enterChatRoom(roomId, request.getSender());
+    headerAccessor.getSessionAttributes().put("username", request.getSender());
+    headerAccessor.getSessionAttributes().put("roomId", roomId);
   }
 
-  /* 채팅 내역 조회 */
-  @GetMapping("/{roomId}")
-  public ResponseEntity<List<ChatMessageResponse>> getChatMessage(@PathVariable Long roomId) {
-    return ResponseEntity.ok(chatMessageService.getMessage(roomId));
-  }
-
-  /* 채팅방 나가기 */
-  @PatchMapping("/exit")
-  public ResponseEntity<String> closeChatRoom(Principal connectedUser) {
-    chatRoomService.exitChatRoom(connectedUser);
-    User user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
-    return ResponseEntity.ok(user.getNickname() + "님이 퇴장하셨습니다.");
-  }
-
-  /* 웹소켓 통신을 위한 예시 */
-  @GetMapping
-  public String chatRoom(Model model) {
-    return "chat"; // chat.html 템플릿을 찾아서 렌더링
+  /* 메시지 전송 */
+  @MessageMapping("/chat.sendMessage")
+  @SendTo("/topic/{roomId}")
+  public ChatMessageResponse sendMessage(@DestinationVariable Long roomId, @Payload ChatMessageRequest request) {
+    return chatMessageService.saveMessage(roomId, request);
   }
 }
