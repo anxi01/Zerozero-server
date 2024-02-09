afab89d09c7da410d10ead5905aec6d40f1a5f7f
anxi01 <tjdals2893@naver.com>
2024-02-09 23:35:00 +0900

    refactor : top10 랭크 구하는 api 제거

--- src/main/java/com/zerozero/domain/user/api/UserController.java
+++ src/main/java/com/zerozero/domain/user/api/UserController.java
@@ -1,14 +1,12 @@
 package com.zerozero.domain.user.api;
 
 import com.zerozero.domain.user.application.UserService;
-import com.zerozero.domain.user.dto.UserStoreRankDTO;
 import com.zerozero.domain.user.dto.response.UserInfoResponse;
 import com.zerozero.global.common.dto.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.security.Principal;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,12 +27,6 @@ public class UserController {
   @GetMapping("/my-page")
   public ApiResponse<UserInfoResponse> getMyInfo(Principal connectedUser) {
     return ApiResponse.ok(userService.getUserInfo(connectedUser));
-  }
-
-  @Operation(summary = "Top10 랭크 조회", description = "판매점 등록을 많이 한 사용자 순으로 10명 조회합니다.")
-  @GetMapping("/rank")
-  public ApiResponse<List<UserStoreRankDTO>> getTop10Users() {
-    return ApiResponse.ok(userService.getTop10UsersByStoreCount());
   }
 
   @Operation(summary = "프로필 사진 업로드", description = "프로필 사진을 업로드합니다.")
