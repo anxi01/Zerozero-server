28454d11a65303c15e2429efcfc20be1ac6a3c23
anxi01 <tjdals2893@naver.com>
2024-03-20 17:30:47 +0900

    feat : 리뷰 등록 api 구현

---
+++ src/main/java/com/zerozero/domain/store/api/ReviewController.java
@@ -0,0 +1,28 @@
+package com.zerozero.domain.store.api;
+
+import com.zerozero.domain.store.application.ReviewService;
+import com.zerozero.domain.store.dto.request.ReviewRequest;
+import com.zerozero.global.common.dto.response.ApiResponse;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/review")
+@RequiredArgsConstructor
+public class ReviewController {
+
+  private final ReviewService reviewService;
+
+  @PostMapping("/{storeId}")
+  private ApiResponse<String> uploadReview(Principal connectedUser, @PathVariable Long storeId,
+      @RequestBody ReviewRequest request) {
+
+    reviewService.uploadReview(connectedUser, storeId, request);
+    return ApiResponse.ok();
+  }
+}
