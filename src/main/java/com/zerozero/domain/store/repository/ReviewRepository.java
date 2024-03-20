4506eb882a80b478aac80fa33f8b711ec8ce392d
anxi01 <tjdals2893@naver.com>
2024-03-20 20:34:16 +0900

    feat : 리뷰 repository에 리뷰 중복 확인 메서드 추가

--- src/main/java/com/zerozero/domain/store/repository/ReviewRepository.java
+++ src/main/java/com/zerozero/domain/store/repository/ReviewRepository.java
@@ -1,8 +1,11 @@
 package com.zerozero.domain.store.repository;
 
 import com.zerozero.domain.store.domain.Review;
+import com.zerozero.domain.store.domain.Store;
+import com.zerozero.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+  boolean existsByUserAndStore(User user, Store store);
 }
