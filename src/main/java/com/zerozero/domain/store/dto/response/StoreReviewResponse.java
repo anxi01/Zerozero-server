e6b5823cea980733ad5a006ac41404f6e748d812
anxi01 <tjdals2893@naver.com>
2024-03-21 00:11:00 +0900

    feat : 판매점 조회 dto에 리뷰 조회 dto 추가

--- src/main/java/com/zerozero/domain/store/dto/response/StoreReviewResponse.java
+++ src/main/java/com/zerozero/domain/store/dto/response/StoreReviewResponse.java
@@ -1,18 +1,35 @@
 package com.zerozero.domain.store.dto.response;
 
+import com.zerozero.domain.store.domain.Review;
 import com.zerozero.domain.store.domain.Store;
+import java.time.LocalDateTime;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreReviewResponse {
 
+  private StoreInfoResponse storeInfo;
+  private List<ReviewResponse> reviews;
+
+  public static StoreReviewResponse of(Store store, List<Review> reviews) {
+    return StoreReviewResponse.builder()
+        .storeInfo(StoreInfoResponse.from(store))
+        .reviews(reviews.stream().map(ReviewResponse::from).toList())
+        .build();
+  }
 
   @Builder
   @Getter
   public static class StoreInfoResponse {
 
-    private Long id;
+    private Long storeId;
 
     private String name;
 
@@ -32,7 +49,7 @@ public class StoreReviewResponse {
 
     public static StoreInfoResponse from(Store store) {
       return StoreInfoResponse.builder()
-          .id(store.getId())
+          .storeId(store.getId())
           .name(store.getName())
           .category(store.getCategory())
           .address(store.getAddress())
@@ -41,6 +58,31 @@ public class StoreReviewResponse {
           .mapy(store.getMapy())
           .selling(store.isSelling())
           .images(store.getImageUrl())
+          .build();
+    }
+  }
+
+  @Getter
+  @Builder
+  public static class ReviewResponse {
+
+    private Long reviewId;
+    private Long userId;
+    private String nickname;
+    private Integer rating;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static ReviewResponse from(Review review) {
+      return ReviewResponse.builder()
+          .reviewId(review.getId())
+          .userId(review.getUser().getId())
+          .nickname(review.getUser().getNickname())
+          .rating(review.getRating())
+          .content(review.getContent())
+          .createdAt(review.getCreatedAt())
+          .updatedAt(review.getUpdatedAt())
           .build();
     }
   }
