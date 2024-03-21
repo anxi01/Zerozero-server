5bc5a4e77ea3c00ef4dc5e3ae41a1355666e3248
anxi01 <tjdals2893@naver.com>
2024-03-22 01:27:47 +0900

    feat : request dto에 제로 음료수 종류 추가

--- src/main/java/com/zerozero/domain/store/dto/request/ReviewRequest.java
+++ src/main/java/com/zerozero/domain/store/dto/request/ReviewRequest.java
@@ -1,9 +1,12 @@
 package com.zerozero.domain.store.dto.request;
 
+import com.zerozero.domain.store.domain.ZeroDrinks;
+import java.util.List;
 import lombok.Getter;
 
 @Getter
 public class ReviewRequest {
 
+  private List<ZeroDrinks> zeroDrinks;
   private String content;
 }
