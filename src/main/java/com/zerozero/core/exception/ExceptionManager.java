bd33d20f6bf78a8ab40de41d9276a38241a14ce3
anxi01 <tjdals2893@naver.com>
2024-07-07 14:50:22 +0900

    FEAT: (#57) 에러코드를 커스텀하고 관리한다

--- src/main/java/com/zerozero/core/exception/ExceptionManager.java
+++ src/main/java/com/zerozero/core/exception/ExceptionManager.java
@@ -1,16 +1,25 @@
 package com.zerozero.core.exception;
 
-import com.zerozero.core.presentation.ApiResponse;
 import com.zerozero.core.presentation.ErrorResponse;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionManager {
 
-  @ExceptionHandler(ServiceException.class)
-  public ApiResponse<?> handleException(ServiceException e) {
-    ErrorResponse errorResponse = ErrorResponse.from(e.getErrorCode());
-    return ApiResponse.fail(errorResponse, errorResponse.getCode());
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> handleException(Exception e) {
+    ErrorResponse errorResponse = ErrorResponse.createErrorCode().statusCode(500).exception(e).build();
+    return ResponseEntity.internalServerError().body(errorResponse);
+  }
+
+  @ExceptionHandler(DomainException.class)
+  public ResponseEntity<ErrorResponse> handleException(DomainException e) {
+    HttpStatus httpStatus = Optional.ofNullable(e.getHttpStatus()).orElse(HttpStatus.INTERNAL_SERVER_ERROR);
+    ErrorResponse errorResponse = ErrorResponse.createDomainErrorCode().statusCode(httpStatus.value()).exception(e).build();
+    return ResponseEntity.status(httpStatus).body(errorResponse);
   }
 }
