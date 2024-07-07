694b44038dc9ab8da9c3aafdf19aa4aacbed271b
anxi01 <tjdals2893@naver.com>
2024-07-07 13:52:34 +0900

    FEAT: (#57) 사용자가 회원가입을 할 수 있다

---
+++ src/main/java/com/zerozero/auth/application/RegisterUserUseCase.java
@@ -0,0 +1,119 @@
+package com.zerozero.auth.application;
+
+import com.zerozero.auth.application.RegisterUserUseCase.RegisterUserRequest;
+import com.zerozero.auth.application.RegisterUserUseCase.RegisterUserResponse;
+import com.zerozero.core.application.BaseRequest;
+import com.zerozero.core.application.BaseResponse;
+import com.zerozero.core.application.BaseUseCase;
+import com.zerozero.core.domain.entity.User;
+import com.zerozero.core.domain.entity.User.Role;
+import com.zerozero.core.domain.infra.repository.UserJPARepository;
+import com.zerozero.core.exception.DomainException;
+import com.zerozero.core.exception.error.BaseErrorCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class RegisterUserUseCase implements BaseUseCase<RegisterUserRequest, RegisterUserResponse> {
+
+  private final PasswordEncoder passwordEncoder;
+
+  private final UserJPARepository userJPARepository;
+
+  @Override
+  public RegisterUserResponse execute(RegisterUserRequest request) {
+    if (request == null || !request.isValid()) {
+      log.error("[RegisterUserUseCase] Invalid register request");
+      return RegisterUserResponse.builder().success(false)
+          .errorCode(RegisterUserErrorCode.NOT_EXIST_REGISTER_CONDITION).build();
+    }
+    if (isDuplicateEmail(request.getEmail())) {
+      log.error("[RegisterUserUseCase] Email already exists");
+      return RegisterUserResponse.builder().success(false)
+          .errorCode(RegisterUserErrorCode.ALREADY_EXIST_EMAIL).build();
+    }
+    if (isDuplicateNickname(request.getNickname())) {
+      log.error("[RegisterUserUseCase] Nickname already exists");
+      return RegisterUserResponse.builder().success(false)
+          .errorCode(RegisterUserErrorCode.ALREADY_EXIST_NICKNAME).build();
+    }
+    User user = User.builder()
+        .nickname(request.getNickname())
+        .email(request.getEmail())
+        .password(passwordEncoder.encode(request.getPassword()))
+        .role(Role.USER)
+        .build();
+    userJPARepository.save(user);
+    return RegisterUserResponse.builder().build();
+  }
+
+  private boolean isDuplicateEmail(String email) {
+    return userJPARepository.existsByEmail(email);
+  }
+
+  private boolean isDuplicateNickname(String nickname) {
+    return userJPARepository.existsByNickname(nickname);
+  }
+
+  @Getter
+  @RequiredArgsConstructor
+  public enum RegisterUserErrorCode implements BaseErrorCode<DomainException> {
+
+    NOT_EXIST_REGISTER_CONDITION(HttpStatus.BAD_REQUEST, "회원가입 조건이 올바르지 않습니다."),
+    ALREADY_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    ALREADY_EXIST_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다.");
+
+    private final HttpStatus httpStatus;
+
+    private final String message;
+
+    @Override
+    public DomainException toException() {
+      return new DomainException(httpStatus, this);
+    }
+  }
+
+  @ToString
+  @Getter
+  @Setter
+  @SuperBuilder
+  @NoArgsConstructor(access = AccessLevel.PROTECTED)
+  public static class RegisterUserResponse extends BaseResponse<RegisterUserErrorCode> {
+  }
+
+  @ToString
+  @Getter
+  @Setter
+  @Builder
+  @NoArgsConstructor(access = AccessLevel.PROTECTED)
+  @AllArgsConstructor(access = AccessLevel.PROTECTED)
+  public static class RegisterUserRequest implements BaseRequest {
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+
+    @Override
+    public boolean isValid() {
+      return nickname != null && email != null && password != null;
+    }
+  }
+
+}
---
+++ src/main/java/com/zerozero/auth/presentation/RegisterUserController.java
@@ -0,0 +1,91 @@
+package com.zerozero.auth.presentation;
+
+import com.zerozero.auth.application.RegisterUserUseCase;
+import com.zerozero.core.application.BaseRequest;
+import com.zerozero.core.application.BaseResponse;
+import com.zerozero.core.exception.error.GlobalErrorCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import java.util.Optional;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "User", description = "사용자")
+public class RegisterUserController {
+
+  private final RegisterUserUseCase registerUserUseCase;
+
+  @Operation(
+      summary = "사용자 회원가입 API",
+      description = "사용자의 개인 정보를 통해 회원을 가입합니다.",
+      operationId = "/register"
+  )
+  @PostMapping("/register")
+  public ResponseEntity<RegisterUserResponse> registerUser(@Valid @RequestBody RegisterUserRequest registerUserRequest) {
+    RegisterUserUseCase.RegisterUserResponse registerUserResponse = registerUserUseCase.execute(
+        RegisterUserUseCase.RegisterUserRequest.builder()
+            .nickname(registerUserRequest.getNickname())
+            .email(registerUserRequest.getEmail())
+            .password(registerUserRequest.getPassword())
+            .build());
+    if (registerUserResponse == null || !registerUserResponse.isSuccess()) {
+      Optional.ofNullable(registerUserResponse)
+          .map(BaseResponse::getErrorCode)
+          .ifPresentOrElse(errorCode -> {
+            throw errorCode.toException();
+          }, () -> {
+            throw GlobalErrorCode.INTERNAL_ERROR.toException();
+          });
+    }
+    return ResponseEntity.ok().build();
+  }
+
+  @ToString
+  @Getter
+  @Setter
+  @SuperBuilder
+  @NoArgsConstructor(access = AccessLevel.PROTECTED)
+  @Schema(description = "사용자 회원가입 응답")
+  public static class RegisterUserResponse extends BaseResponse<GlobalErrorCode> {
+  }
+
+  @ToString
+  @Getter
+  @Setter
+  @Builder
+  @NoArgsConstructor(access = AccessLevel.PROTECTED)
+  @AllArgsConstructor(access = AccessLevel.PROTECTED)
+  @Schema(description = "사용자 회원가입 요청")
+  public static class RegisterUserRequest implements BaseRequest {
+
+    @NotNull(message = "닉네임은 필수 데이터입니다.")
+    @Schema(description = "닉네임", example = "제로")
+    private String nickname;
+
+    @NotNull(message = "이메일은 필수 데이터입니다.")
+    @Email
+    @Schema(description = "이메일", example = "zerozero@drink.com")
+    private String email;
+
+    @NotNull(message = "비밀번호는 필수 데이터입니다.")
+    @Schema(description = "비밀번호", example = "zero")
+    private String password;
+  }
+}
