a46691d2f883b65856ff23886422c25b10e05222
anxi01 <tjdals2893@naver.com>
2024-07-07 14:50:22 +0900

    FEAT: (#57) 에러코드를 커스텀하고 관리한다

--- src/main/java/com/zerozero/core/exception/DomainException.java
+++ src/main/java/com/zerozero/core/exception/DomainException.java
@@ -1,10 +1,28 @@
 package com.zerozero.core.exception;
 
-import com.zerozero.core.exception.error.ErrorCode;
-import lombok.AllArgsConstructor;
+import com.zerozero.core.exception.error.BaseErrorCode;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
-public class ServiceException extends RunTimeCode {
-  private final ErrorCode errorCode;
+public class DomainException extends RuntimeException {
+
+  private HttpStatus httpStatus;
+
+  private String code;
+
+  public DomainException(String message) {
+    super(message);
+  }
+
+  public DomainException(HttpStatus httpStatus, String message) {
+    super(message);
+    this.httpStatus = httpStatus;
+  }
+
+  public DomainException(HttpStatus httpStatus, BaseErrorCode<?> errorCode) {
+    super(errorCode.getMessage());
+    this.httpStatus = httpStatus;
+    this.code = errorCode.name();
+  }
 }
