7d82d55e13f55bb0872f1b61169c32e05f56662e
anxi01 <tjdals2893@naver.com>
2024-12-03 15:09:20 +0900

    FEAT: (#111) Sentry를 사용하여 에러 모니터링 환경을 구축한다

--- src/main/java/com/zerozero/core/exception/ExceptionManager.java
+++ src/main/java/com/zerozero/core/exception/ExceptionManager.java
@@ -2,6 +2,8 @@ package com.zerozero.core.exception;
 
 import com.zerozero.core.presentation.ErrorResponse;
 import java.util.Optional;
+
+import io.sentry.Sentry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +15,7 @@ public class ExceptionManager {
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponse> handleException(Exception e) {
     ErrorResponse errorResponse = ErrorResponse.createErrorCode().statusCode(500).exception(e).build();
+    Sentry.captureException(e);
     return ResponseEntity.internalServerError().body(errorResponse);
   }
 
@@ -20,6 +23,7 @@ public class ExceptionManager {
   public ResponseEntity<ErrorResponse> handleException(DomainException e) {
     HttpStatus httpStatus = Optional.ofNullable(e.getHttpStatus()).orElse(HttpStatus.INTERNAL_SERVER_ERROR);
     ErrorResponse errorResponse = ErrorResponse.createDomainErrorCode().statusCode(httpStatus.value()).exception(e).build();
+    Sentry.captureException(e);
     return ResponseEntity.status(httpStatus).body(errorResponse);
   }
 }
