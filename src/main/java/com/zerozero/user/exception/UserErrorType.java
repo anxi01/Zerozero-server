09e1ba904fb687d0c2159e350730a9c877cf32db
anxi01 <tjdals2893@naver.com>
2025-02-20 16:55:06 +0900

    FIX: (#145) 사용자 도메인 예외 계층을 추가한다

---
+++ src/main/java/com/zerozero/user/exception/UserErrorType.java
@@ -0,0 +1,19 @@
+package com.zerozero.user.exception;
+
+import com.zerozero.core.support.error.ErrorType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorType implements ErrorType {
+    NOT_EXIST_USER(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+    NOT_COMPLETED_MEMBER(HttpStatus.FORBIDDEN, "회원가입이 완료되지 않은 사용자입니다."),
+    ALREADY_REGISTERED_USER(HttpStatus.BAD_REQUEST, "이미 회원가입이 완료된 사용자입니다."),
+    ;
+
+    private final HttpStatus status;
+
+    private final String message;
+}
