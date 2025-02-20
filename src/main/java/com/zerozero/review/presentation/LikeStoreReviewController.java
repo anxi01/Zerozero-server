59d57e1cf32ee3b8c2b41ce766478063b1ff214e
anxi01 <tjdals2893@naver.com>
2025-02-20 16:50:32 +0900

    FIX: (#145) 리뷰 도메인 표현 계층을 재정의한다

--- src/main/java/com/zerozero/review/presentation/LikeStoreReviewController.java
+++ src/main/java/com/zerozero/review/presentation/LikeStoreReviewController.java
@@ -3,23 +3,20 @@ package com.zerozero.review.presentation;
 import com.zerozero.configuration.argumentresolver.LoginUser;
 import com.zerozero.configuration.interceptor.Authorization;
 import com.zerozero.configuration.swagger.ApiErrorCode;
-import com.zerozero.core.application.BaseResponse;
-import com.zerozero.core.domain.entity.User;
-import com.zerozero.core.exception.error.GlobalErrorCode;
-import com.zerozero.review.application.LikeStoreReviewUseCase;
-import com.zerozero.review.application.LikeStoreReviewUseCase.LikeStoreReviewErrorCode;
+import com.zerozero.core.support.error.GlobalErrorType;
+import com.zerozero.core.support.response.ApiResponse;
+import com.zerozero.review.domain.service.LikeStoreReviewUseCase;
+import com.zerozero.review.exception.ReviewErrorType;
+import com.zerozero.user.domain.model.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -27,42 +24,20 @@ import java.util.UUID;
 @Tag(name = "Review", description = "리뷰")
 public class LikeStoreReviewController {
 
-  private final LikeStoreReviewUseCase likeStoreReviewUseCase;
+    private final LikeStoreReviewUseCase likeStoreReviewUseCase;
 
-  @Operation(
-      summary = "리뷰 좋아요 API",
-      description = "사용자가 리뷰 ID를 통해 좋아요를 누릅니다.",
-      operationId = "/review/like/{reviewId}"
-  )
-  @ApiErrorCode({GlobalErrorCode.class, LikeStoreReviewErrorCode.class})
-  @Authorization
-  @PatchMapping("/review/like/{reviewId}")
-  public ResponseEntity<LikeStoreReviewResponse> likeStoreReview(@PathVariable(name = "reviewId") @Schema(description = "리뷰 ID") UUID reviewId,
-                                                                 @Parameter(hidden = true) @LoginUser User user) {
-    LikeStoreReviewUseCase.LikeStoreReviewResponse likeStoreReviewResponse = likeStoreReviewUseCase.execute(
-        LikeStoreReviewUseCase.LikeStoreReviewRequest.builder()
-            .reviewId(reviewId)
-            .user(user)
-            .build());
-    if (likeStoreReviewResponse == null || !likeStoreReviewResponse.isSuccess()) {
-      Optional.ofNullable(likeStoreReviewResponse)
-          .map(BaseResponse::getErrorCode)
-          .ifPresentOrElse(errorCode -> {
-            throw errorCode.toException();
-          }, () -> {
-            throw GlobalErrorCode.INTERNAL_ERROR.toException();
-          });
+    @Operation(
+            summary = "리뷰 좋아요 API",
+            description = "사용자가 리뷰 ID를 통해 좋아요를 누릅니다.",
+            operationId = "/review/like/{reviewId}"
+    )
+    @ApiErrorCode({GlobalErrorType.class, ReviewErrorType.class})
+    @Authorization
+    @PatchMapping("/review/like/{reviewId}")
+    public ApiResponse<?> likeStoreReview(@PathVariable(name = "reviewId") @Schema(description = "리뷰 ID") UUID reviewId,
+                                          @Parameter(hidden = true) @LoginUser User user) {
+        likeStoreReviewUseCase.execute(reviewId, user);
+        return ApiResponse.success();
     }
-    return ResponseEntity.ok().build();
-  }
-
-  @ToString
-  @Getter
-  @Setter
-  @SuperBuilder
-  @NoArgsConstructor(access = AccessLevel.PROTECTED)
-  @Schema(description = "리뷰 좋아요 응답")
-  public static class LikeStoreReviewResponse extends BaseResponse<GlobalErrorCode> {
-  }
 
 }
--- src/main/java/com/zerozero/review/presentation/UpdateStoreReviewController.java
+++ src/main/java/com/zerozero/review/presentation/UpdateStoreReviewController.java
@@ -3,28 +3,23 @@ package com.zerozero.review.presentation;
 import com.zerozero.configuration.argumentresolver.LoginUser;
 import com.zerozero.configuration.interceptor.Authorization;
 import com.zerozero.configuration.swagger.ApiErrorCode;
-import com.zerozero.core.application.BaseRequest;
-import com.zerozero.core.application.BaseResponse;
-import com.zerozero.core.domain.entity.User;
-import com.zerozero.core.domain.vo.ZeroDrink;
-import com.zerozero.core.domain.vo.ZeroDrink.Type;
-import com.zerozero.core.exception.error.GlobalErrorCode;
-import com.zerozero.review.application.UpdateStoreReviewUseCase;
-import com.zerozero.review.application.UpdateStoreReviewUseCase.UpdateStoreReviewErrorCode;
+import com.zerozero.core.support.error.GlobalErrorType;
+import com.zerozero.core.support.response.ApiResponse;
+import com.zerozero.review.domain.service.UpdateStoreReviewUseCase;
+import com.zerozero.review.exception.ReviewErrorType;
+import com.zerozero.review.presentation.request.ReviewRequest;
+import com.zerozero.user.domain.model.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
-import org.springframework.http.ResponseEntity;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -32,65 +27,21 @@ import java.util.UUID;
 @Tag(name = "Review", description = "리뷰")
 public class UpdateStoreReviewController {
 
-  private final UpdateStoreReviewUseCase updateStoreReviewUseCase;
+    private final UpdateStoreReviewUseCase updateStoreReviewUseCase;
 
-  @Operation(
-      summary = "리뷰 수정 API",
-      description = "리뷰 ID를 통해 리뷰를 수정합니다.",
-      operationId = "/review/{reviewId}"
-  )
-  @ApiErrorCode({GlobalErrorCode.class, UpdateStoreReviewErrorCode.class})
-  @Authorization
-  @PatchMapping("/review/{reviewId}")
-  public ResponseEntity<UpdateStoreReviewResponse> updateStoreReview(@PathVariable(name = "reviewId") @Schema(description = "리뷰 ID") UUID reviewId,
-                                                                     @RequestBody UpdateStoreReviewRequest request,
-                                                                     @Parameter(hidden = true) @LoginUser User user) {
-    UpdateStoreReviewUseCase.UpdateStoreReviewResponse updateStoreReviewResponse = updateStoreReviewUseCase.execute(
-        UpdateStoreReviewUseCase.UpdateStoreReviewRequest.builder()
-            .reviewId(reviewId)
-            .content(request.getContent())
-            .zeroDrinks(Optional.ofNullable(request.getZeroDrinks())
-                .map(zeroDrinks -> zeroDrinks.stream().map(zeroDrink -> ZeroDrink.builder()
-                        .type(zeroDrink)
-                        .build())
-                    .toArray(ZeroDrink[]::new))
-                .orElse(null))
-            .user(user)
-            .build());
-    if (updateStoreReviewResponse == null || !updateStoreReviewResponse.isSuccess()) {
-      Optional.ofNullable(updateStoreReviewResponse)
-          .map(BaseResponse::getErrorCode)
-          .ifPresentOrElse(errorCode -> {
-            throw errorCode.toException();
-          }, () -> {
-            throw GlobalErrorCode.INTERNAL_ERROR.toException();
-          });
+    @Operation(
+            summary = "리뷰 수정 API",
+            description = "리뷰 ID를 통해 리뷰를 수정합니다.",
+            operationId = "/review/{reviewId}"
+    )
+    @ApiErrorCode({GlobalErrorType.class, ReviewErrorType.class})
+    @Authorization
+    @PatchMapping("/review/{reviewId}")
+    public ApiResponse<?> updateStoreReview(@PathVariable(name = "reviewId") @Schema(description = "리뷰 ID") UUID reviewId,
+                                            @Valid @RequestBody ReviewRequest reviewRequest,
+                                            @Parameter(hidden = true) @LoginUser User user) {
+        updateStoreReviewUseCase.execute(reviewId, reviewRequest, user);
+        return ApiResponse.success();
     }
-    return ResponseEntity.ok().build();
-  }
 
-  @ToString
-  @Getter
-  @Setter
-  @SuperBuilder
-  @NoArgsConstructor(access = AccessLevel.PROTECTED)
-  @Schema(description = "리뷰 수정 웅답")
-  public static class UpdateStoreReviewResponse extends BaseResponse<GlobalErrorCode> {
-  }
-
-  @ToString
-  @Getter
-  @Setter
-  @Builder
-  @NoArgsConstructor(access = AccessLevel.PROTECTED)
-  @AllArgsConstructor(access = AccessLevel.PROTECTED)
-  @Schema(description = "리뷰 수정 요청")
-  public static class UpdateStoreReviewRequest implements BaseRequest {
-
-    @Schema(description = "리뷰 내용", example = "제로콜라 판매 중!")
-    private String content;
-
-    @Schema(description = "제로 음료수 목록", example = "[\"COCA_COLA_ZERO\", \"PEPSI_ZERO\", \"SPRITE_ZERO\"]")
-    private List<Type> zeroDrinks;
-  }
 }
