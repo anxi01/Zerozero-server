dc5c6176ed8a7675d8c04ba5e76af9d0640fc497
anxi01 <tjdals2893@naver.com>
2025-02-20 16:55:29 +0900

    FIX: (#145) 사용자 도메인 표현 계층을 재정의한다

--- src/main/java/com/zerozero/user/presentation/ReadUserInfoController.java
+++ src/main/java/com/zerozero/user/presentation/ReadUserInfoController.java
@@ -3,72 +3,35 @@ package com.zerozero.user.presentation;
 import com.zerozero.configuration.argumentresolver.LoginUser;
 import com.zerozero.configuration.interceptor.Authorization;
 import com.zerozero.configuration.swagger.ApiErrorCode;
-import com.zerozero.core.application.BaseResponse;
-import com.zerozero.core.domain.entity.User;
-import com.zerozero.core.domain.record.UserProfile;
-import com.zerozero.core.exception.error.GlobalErrorCode;
-import com.zerozero.user.application.ReadUserInfoUseCase;
-import com.zerozero.user.application.ReadUserInfoUseCase.ReadUserInfoErrorCode;
+import com.zerozero.core.support.error.GlobalErrorType;
+import com.zerozero.core.support.response.ApiResponse;
+import com.zerozero.user.application.UserService;
+import com.zerozero.user.domain.model.User;
+import com.zerozero.user.exception.UserErrorType;
+import com.zerozero.user.presentation.response.ReadUserInfoResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "User", description = "사용자")
 public class ReadUserInfoController {
 
-  private final ReadUserInfoUseCase readUserInfoUseCase;
+    private final UserService userService;
 
-  @Operation(
-      summary = "마이페이지 조회 API",
-      description = "사용자의 마이페이지를 토큰을 통해 조회합니다.",
-      operationId = "/user/mypage"
-  )
-  @ApiErrorCode({GlobalErrorCode.class, ReadUserInfoErrorCode.class})
-  @Authorization
-  @GetMapping("/user/mypage")
-  public ResponseEntity<ReadUserInfoResponse> readUserInfo(@Parameter(hidden = true) @LoginUser User user) {
-    ReadUserInfoUseCase.ReadUserInfoResponse readUserInfoResponse = readUserInfoUseCase.execute(
-        ReadUserInfoUseCase.ReadUserInfoRequest.builder()
-            .user(user)
-            .build());
-    if (readUserInfoResponse == null || !readUserInfoResponse.isSuccess()) {
-      Optional.ofNullable(readUserInfoResponse)
-          .map(BaseResponse::getErrorCode)
-          .ifPresentOrElse(errorCode -> {
-            throw errorCode.toException();
-          }, () -> {
-            throw GlobalErrorCode.INTERNAL_ERROR.toException();
-          });
+    @Operation(
+            summary = "마이페이지 조회 API",
+            description = "사용자의 마이페이지를 토큰을 통해 조회합니다.",
+            operationId = "/user/mypage"
+    )
+    @ApiErrorCode({GlobalErrorType.class, UserErrorType.class})
+    @Authorization
+    @GetMapping("/user/mypage")
+    public ApiResponse<ReadUserInfoResponse> readUserInfo(@Parameter(hidden = true) @LoginUser User user) {
+        return ApiResponse.success(userService.readUserInfo(user));
     }
-    return ResponseEntity.ok(ReadUserInfoResponse.builder()
-        .userProfile(Optional.ofNullable(readUserInfoResponse.getUserProfile())
-            .map(
-                userProfile -> new UserProfile(userProfile.getNickname(),
-                    userProfile.getProfileImage(),
-                    userProfile.getRank(), userProfile.getStoreReportCount())).orElse(null)).build()
-    );
-  }
-
-  @ToString
-  @Getter
-  @Setter
-  @SuperBuilder
-  @NoArgsConstructor(access = AccessLevel.PROTECTED)
-  @AllArgsConstructor(access = AccessLevel.PROTECTED)
-  @Schema(description = "사용자 마이페이지 조회 응답")
-  public static class ReadUserInfoResponse extends BaseResponse<GlobalErrorCode> {
-
-    @Schema(description = "사용자 정보")
-    private UserProfile userProfile;
-  }
 }
--- src/main/java/com/zerozero/user/presentation/UpdateUserProfileController.java
+++ src/main/java/com/zerozero/user/presentation/UpdateUserProfileController.java
@@ -3,31 +3,21 @@ package com.zerozero.user.presentation;
 import com.zerozero.configuration.argumentresolver.LoginUser;
 import com.zerozero.configuration.interceptor.Authorization;
 import com.zerozero.configuration.swagger.ApiErrorCode;
-import com.zerozero.core.application.BaseRequest;
-import com.zerozero.core.application.BaseResponse;
-import com.zerozero.core.domain.entity.User;
-import com.zerozero.core.exception.error.GlobalErrorCode;
-import com.zerozero.user.application.UpdateUserProfileUseCase;
-import com.zerozero.user.application.UpdateUserProfileUseCase.UpdateUserProfileErrorCode;
+import com.zerozero.core.support.error.GlobalErrorType;
+import com.zerozero.core.support.response.ApiResponse;
+import com.zerozero.user.domain.model.User;
+import com.zerozero.user.domain.service.UpdateUserUseCase;
+import com.zerozero.user.exception.UserErrorType;
+import com.zerozero.user.presentation.request.UpdateUserRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import java.util.Optional;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.experimental.SuperBuilder;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,60 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "User", description = "사용자")
 public class UpdateUserProfileController {
 
-  private final UpdateUserProfileUseCase updateUserProfileUseCase;
+    private final UpdateUserUseCase updateUserUseCase;
 
-  @Operation(
-      summary = "프로필 수정 API",
-      description = "사용자가 이미지 파일과 닉네임을 변경할 수 있습니다.",
-      operationId = "/user"
-  )
-  @ApiErrorCode({GlobalErrorCode.class, UpdateUserProfileErrorCode.class})
-  @Authorization
-  @PatchMapping("/user")
-  public ResponseEntity<UpdateUserProfileResponse> uploadProfileImage(
-      @Valid @RequestBody UpdateUserProfileRequest request,
-      @Parameter(hidden = true) @LoginUser User user) {
-    UpdateUserProfileUseCase.UpdateUserProfileResponse updateUserProfileResponse = updateUserProfileUseCase.execute(
-        UpdateUserProfileUseCase.UpdateUserProfileRequest.builder()
-            .nickname(request.getNickname())
-            .image(request.getImage())
-            .user(user)
-            .build());
-    if (updateUserProfileResponse == null || !updateUserProfileResponse.isSuccess()) {
-      Optional.ofNullable(updateUserProfileResponse)
-          .map(BaseResponse::getErrorCode)
-          .ifPresentOrElse(errorCode -> {
-            throw errorCode.toException();
-          }, () -> {
-            throw GlobalErrorCode.INTERNAL_ERROR.toException();
-          });
+    @Operation(
+            summary = "프로필 수정 API",
+            description = "사용자가 이미지와 닉네임을 변경할 수 있습니다.",
+            operationId = "/user"
+    )
+    @ApiErrorCode({GlobalErrorType.class, UserErrorType.class})
+    @Authorization
+    @PatchMapping("/user")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<?> uploadProfileImage(@Valid @RequestBody UpdateUserRequest updateUserRequest, @Parameter(hidden = true) @LoginUser User user) {
+        updateUserUseCase.execute(updateUserRequest, user);
+        return ApiResponse.success();
     }
-    return ResponseEntity.ok(UpdateUserProfileResponse.builder().build());
-  }
-
-  @ToString
-  @Getter
-  @Setter
-  @SuperBuilder
-  @NoArgsConstructor(access = AccessLevel.PROTECTED)
-  @Schema(description = "프로필 수정 응답")
-  public static class UpdateUserProfileResponse extends BaseResponse<GlobalErrorCode> {
-  }
-
-  @ToString
-  @Getter
-  @Setter
-  @Builder
-  @NoArgsConstructor(access = AccessLevel.PROTECTED)
-  @AllArgsConstructor(access = AccessLevel.PROTECTED)
-  @Schema(description = "프로필 수정 요청")
-  public static class UpdateUserProfileRequest implements BaseRequest {
-
-    @NotNull(message = "닉네임은 필수 데이터입니다.")
-    @Schema(description = "닉네임", example = "제로")
-    private String nickname;
-
-    @Schema(description = "판매점 업로드 이미지 URL", example = "https://s3.ap-northeast-2.amazonaws.com/zerozero-upload/images/store/0cbf3b99-b0ba-4148-a891-d04cb71ae236-test.png")
-    private String image;
-  }
 }
