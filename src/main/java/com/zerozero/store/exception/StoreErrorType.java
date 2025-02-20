79c127238be5e5af387e7e28addc756d1a97647e
anxi01 <tjdals2893@naver.com>
2025-02-20 16:52:34 +0900

    FIX: (#145) 판매점 도메인 예외 계층을 추가한다

---
+++ src/main/java/com/zerozero/store/exception/StoreErrorType.java
@@ -0,0 +1,19 @@
+package com.zerozero.store.exception;
+
+import com.zerozero.core.support.error.ErrorType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum StoreErrorType implements ErrorType {
+    NOT_EXIST_STORE(HttpStatus.BAD_REQUEST, "판매점이 존재하지 않습니다."),
+    NOT_EXIST_SEARCH_RESPONSE(HttpStatus.BAD_REQUEST, "검색 응답이 존재하지 않습니다."),
+    KAKAO_SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "KAKAO REST API가 동작하지 않습니다."),
+    ;
+
+    private final HttpStatus status;
+
+    private final String message;
+}
