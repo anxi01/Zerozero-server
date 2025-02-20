4718535342a3f7a67887b1db8c8343ed72d000dd
anxi01 <tjdals2893@naver.com>
2025-02-20 16:49:11 +0900

    FIX: (#145) 리뷰 도메인 도메인 계층을 재정의한다

---
+++ src/main/java/com/zerozero/review/domain/service/LikeStoreReviewUseCase.java
@@ -0,0 +1,41 @@
+package com.zerozero.review.domain.service;
+
+import com.zerozero.review.domain.model.Review;
+import com.zerozero.review.domain.model.ReviewLike;
+import com.zerozero.review.domain.repository.ReviewLikeRepository;
+import com.zerozero.review.domain.repository.ReviewRepository;
+import com.zerozero.review.exception.ReviewErrorType;
+import com.zerozero.review.exception.ReviewException;
+import com.zerozero.user.domain.model.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Log4j2
+public class LikeStoreReviewUseCase {
+
+    private final ReviewRepository reviewRepository;
+
+    private final ReviewLikeRepository reviewLikeRepository;
+
+    public void execute(UUID reviewId, User user) {
+        Review review = reviewRepository.findByIdAndDeleted(reviewId, false)
+                .orElseThrow(() -> new ReviewException(ReviewErrorType.NOT_EXIST_DELETABLE_REVIEW));
+        ReviewLike reviewLike = reviewLikeRepository.findByReviewIdAndUserIdAndDeleted(review.getId(), user.getId(), false);
+        if (reviewLike == null) {
+            log.info("[Like] User {} liked Review {}", user.getId(), review.getId());
+            reviewLike = new ReviewLike(review.getId(), user.getId());
+            reviewLikeRepository.save(reviewLike);
+        } else {
+            log.info("[Unlike] User {} unliked Review {}", user.getId(), review.getId());
+            reviewLikeRepository.delete(reviewLike);
+        }
+    }
+
+}
