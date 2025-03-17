7bec9baf20b1b340713e19e0b405cd15888b24e2
anxi01 <tjdals2893@naver.com>
2025-03-17 20:57:00 +0900

    FIX: (NONE) User 테이블명을 변경한다

--- src/main/java/com/zerozero/user/domain/model/User.java
+++ src/main/java/com/zerozero/user/domain/model/User.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@Table(name = "users")
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
