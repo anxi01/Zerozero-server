b4c2b67e64b5f1069e9f5631fe82b8e0456df88f
anxi01 <tjdals2893@naver.com>
2025-03-20 00:32:04 +0900

    REFACTOR: (#147) 카카오 검색 요청, 응답 변수명을 변경한다

--- src/main/java/com/zerozero/store/infrastructure/kakao/search/request/KakaoSearchRequest.java
+++ src/main/java/com/zerozero/store/infrastructure/kakao/search/request/KakaoSearchRequest.java
@@ -2,54 +2,18 @@ package com.zerozero.store.infrastructure.kakao.search.request;
 
 import com.zerozero.store.infrastructure.kakao.search.core.CategoryGroupCode;
 import lombok.Builder;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import lombok.Getter;
 
+@Getter
 @Builder
-public record KakaoSearchRequest(
-        String query,
-        CategoryGroupCode categoryGroupCode,
-        String longitude,
-        String latitude,
-        Integer radius,
-        String rect,
-        Integer page,
-        Integer size,
-        String sort
-) {
-
-    public MultiValueMap<String, String> createQueryParams() {
-        LinkedMultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
-
-        if (query != null && !query.isEmpty()) {
-            queryParams.add("query", query);
-        }
-        if (categoryGroupCode != null) {
-            queryParams.add("category_group_code", categoryGroupCode.name());
-        }
-        if (longitude != null && !longitude.isEmpty()) {
-            queryParams.add("x", longitude);
-        }
-        if (latitude != null && !latitude.isEmpty()) {
-            queryParams.add("y", latitude);
-        }
-        if (radius != null) {
-            queryParams.add("radius", String.valueOf(radius));
-        }
-        if (rect != null && !rect.isEmpty()) {
-            queryParams.add("rect", rect);
-        }
-        if (page != null) {
-            queryParams.add("page", String.valueOf(page));
-        }
-        if (size != null) {
-            queryParams.add("size", String.valueOf(size));
-        }
-        if (sort != null && !sort.isEmpty()) {
-            queryParams.add("sort", sort);
-        }
-
-        return queryParams;
-    }
+public class KakaoSearchRequest {
+    private String query;
+    private CategoryGroupCode categoryGroupCode;
+    private String x;
+    private String y;
+    private Integer radius;
+    private String rect;
+    private Integer page;
+    private Integer size;
+    private String sort;
 }
-
