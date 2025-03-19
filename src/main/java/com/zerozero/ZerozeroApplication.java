a56983375adcce61bf338d7531e03e736e594f42
anxi01 <tjdals2893@naver.com>
2025-03-19 23:46:42 +0900

    FEAT: (#147) OpenFeign 의존성을 추가한다

--- src/main/java/com/zerozero/ZerozeroApplication.java
+++ src/main/java/com/zerozero/ZerozeroApplication.java
@@ -2,9 +2,11 @@ package com.zerozero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 public class ZerozeroApplication {
 
