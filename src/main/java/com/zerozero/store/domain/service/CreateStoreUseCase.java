6ce1f1b3c2f17121ed0b345162eea421e6c74a15
anxi01 <tjdals2893@naver.com>
2025-04-02 20:41:33 +0900

    FIX: (#160) 카카오에서 제공하는 장소 ID를 통해 판매점을 등록하도록 변경한다

--- src/main/java/com/zerozero/store/domain/service/CreateStoreUseCase.java
+++ src/main/java/com/zerozero/store/domain/service/CreateStoreUseCase.java
@@ -3,9 +3,6 @@ package com.zerozero.store.domain.service;
 import com.zerozero.store.domain.event.StoreCreatedEvent;
 import com.zerozero.store.domain.model.Store;
 import com.zerozero.store.domain.repository.StoreRepository;
-import com.zerozero.store.domain.response.StoreSearchResponse;
-import com.zerozero.store.exception.StoreErrorType;
-import com.zerozero.store.exception.StoreException;
 import com.zerozero.store.presentation.request.CreateStoreRequest;
 import com.zerozero.user.domain.model.User;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +10,6 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -23,22 +19,10 @@ public class CreateStoreUseCase {
 
     private final StoreRepository storeRepository;
 
-    private final StoreSearcher storeSearcher;
-
     private final ApplicationEventPublisher eventPublisher;
 
     public UUID execute(CreateStoreRequest createStoreRequest, User user) {
-        List<StoreSearchResponse> storeSearchResponses = storeSearcher.search(createStoreRequest.placeName());
-
-        StoreSearchResponse storeSearchResponse = storeSearchResponses.stream()
-                .filter(store ->
-                        createStoreRequest.placeName().equals(store.placeName()) &&
-                                createStoreRequest.longitude().equals(store.longitude()) &&
-                                createStoreRequest.latitude().equals(store.latitude()))
-                .findFirst()
-                .orElseThrow(() -> new StoreException(StoreErrorType.NOT_EXIST_STORE));
-
-        Store store = Store.create(user.getId(), storeSearchResponse, createStoreRequest.images());
+        Store store = Store.create(user.getId(), createStoreRequest);
         storeRepository.save(store);
 
         UUID storeId = store.getId();
