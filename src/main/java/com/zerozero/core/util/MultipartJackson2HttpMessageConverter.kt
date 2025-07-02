b691f09647b443fda1850295053cb27463954ff1
anxi01 <tjdals2893@naver.com>
2025-07-02 23:19:40 +0900

    FIX: (#171) TimeUtil을 Kotlin으로 전환한다

---
+++ src/main/java/com/zerozero/core/util/MultipartJackson2HttpMessageConverter.kt
@@ -0,0 +1,29 @@
+package com.zerozero.core.util
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.MediaType
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter
+import org.springframework.lang.Nullable
+import org.springframework.stereotype.Component
+import java.lang.reflect.Type
+
+@Component
+class MultipartJackson2HttpMessageConverter(
+    objectMapper: ObjectMapper
+) : AbstractJackson2HttpMessageConverter(objectMapper, MediaType.APPLICATION_OCTET_STREAM) {
+
+    /**
+     * Converter for support http request with header Content-Type: multipart/form-data
+     */
+    override fun canWrite(clazz: Class<*>, @Nullable mediaType: MediaType?): Boolean {
+        return false
+    }
+
+    override fun canWrite(@Nullable type: Type?, clazz: Class<*>, @Nullable mediaType: MediaType?): Boolean {
+        return false
+    }
+
+    override fun canWrite(@Nullable mediaType: MediaType?): Boolean {
+        return false
+    }
+}
