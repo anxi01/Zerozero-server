15b4b4a61c9d3c6f498331a66200eeb9de49d9ae
anxi01 <tjdals2893@naver.com>
2025-07-08 17:03:41 +0900

    FIX: (#173) Image Controller, DTO를 Kotlin으로 전환한다

---
+++ src/main/java/com/zerozero/image/presentation/response/PreSignedUrlResponse.kt
@@ -0,0 +1,23 @@
+package com.zerozero.image.presentation.response
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@JvmRecord
+data class PreSignedUrlResponse(
+    @Schema(
+        description = "이미지 업로드 PreSigned URL",
+        example = "https://s3.ap-northeast-2.amazonaws.com/zerozero-upload/images/store/0cbf3b99-b0ba-4148-a891-d04cb71ae236-test.png"
+    )
+    val preSignedUrl: String,
+
+    @Schema(
+        description = "이미지 객체 URL",
+        example = "https://s3.ap-northeast-2.amazonaws.com/zerozero-upload/images/store/0cbf3b99-b0ba-4148-a891-d04cb71ae236-test.png"
+    )
+    val objectUrl: String
+) {
+    companion object {
+        @JvmStatic
+        fun of(preSignedUrl: String, objectUrl: String) = PreSignedUrlResponse(preSignedUrl, objectUrl)
+    }
+}
