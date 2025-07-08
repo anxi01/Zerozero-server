37f28ec994e8a0b3580d4df0f63bceadcf12c4ae
anxi01 <tjdals2893@naver.com>
2025-07-08 19:14:17 +0900

    FIX: (#173) User Controller, DTO를 Kotlin으로 전환한다

---
+++ src/main/java/com/zerozero/user/presentation/UpdateUserProfileController.kt
@@ -0,0 +1,46 @@
+package com.zerozero.user.presentation
+
+import com.zerozero.configuration.argumentresolver.LoginUser
+import com.zerozero.configuration.interceptor.Authorization
+import com.zerozero.configuration.swagger.ApiErrorCode
+import com.zerozero.core.support.error.GlobalErrorType
+import com.zerozero.core.support.response.ApiResponse
+import com.zerozero.user.domain.model.User
+import com.zerozero.user.domain.service.UpdateUserUseCase
+import com.zerozero.user.exception.UserErrorType
+import com.zerozero.user.presentation.request.UpdateUserRequest
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@Tag(name = "User", description = "사용자")
+@RestController
+class UpdateUserProfileController(
+    private val updateUserUseCase: UpdateUserUseCase
+) {
+
+    @Operation(
+        summary = "프로필 수정 API",
+        description = "사용자가 이미지와 닉네임을 변경할 수 있습니다.",
+        operationId = "/user"
+    )
+    @ApiErrorCode(
+        GlobalErrorType::class, UserErrorType::class
+    )
+    @Authorization
+    @PatchMapping("/user")
+    @ResponseStatus(HttpStatus.OK)
+    fun uploadProfileImage(
+        @Valid @RequestBody updateUserRequest: UpdateUserRequest,
+        @Parameter(hidden = true) @LoginUser user: User
+    ): ApiResponse<Any> {
+        updateUserUseCase.execute(updateUserRequest, user)
+        return ApiResponse.success()
+    }
+}
