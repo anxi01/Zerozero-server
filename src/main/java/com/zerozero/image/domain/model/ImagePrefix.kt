8b284a94916ce9b9f56fc81003c269d73adc3503
anxi01 <tjdals2893@naver.com>
2025-07-13 02:41:55 +0900

    FIX: (#175) Image 응용 서비스를 Kotlin으로 전환한다

--- src/main/java/com/zerozero/image/domain/model/ImagePrefix.kt
+++ src/main/java/com/zerozero/image/domain/model/ImagePrefix.kt
@@ -6,7 +6,6 @@ enum class ImagePrefix {
     ;
 
     companion object {
-        @JvmStatic
         fun fromString(value: String): ImagePrefix? {
             return entries.firstOrNull { it.name.equals(value, ignoreCase = true) }
         }
